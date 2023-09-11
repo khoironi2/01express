af817c82af82276457ee01de4e09ab029c237d99
pak  aji <profkhoironi@gmail.com>
2023-09-11 19:34:20 +0700

    00

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')// membuat varibale baru dengan nama express dan nilainya kita memanggil Express.js
+const app = express() // membuat variable baru dengan nama app yang isinya variable express
+const port = 3000 // membuat variable dengan nama port yang isinya 3000 port ini yang akan kita gunakan untuk menjalankan express
+
+//membuat route baru dengan method GET yang isinya kalimat halo dek
+app.get('/', (req, res) => {
+    res.send('Halo dek')
+})
+
+// kita listen Express.js kedalam port yang kita buat diatas
+app.listen(port, () => {
+    //dan kita tampilkan log sebagai penanda bahawa Express,js  berhasil dijalan kan di port 3000
+    console.log(`aplikasi berjalan di http:://localhost:${port}`)
+})
